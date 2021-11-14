a583429027fa220aa80dbaecca195b23f85538d7
Eduardo Russo <russoedu@gmail.com>
2021-11-14 08:08:27 +0000

    Improved the sheet with a better size and positioning

--- src/pages/ColorsSheet.tsx
+++ src/pages/ColorsSheet.tsx
@@ -1,32 +1,32 @@
 import generator from 'sequential-id-generator'
 import fontColorContrast from 'font-color-contrast'
 import './ColorsSheet.css'
-// import { Slider } from '@mui/material'
 import { Slider } from '../components/Slider'
 import { Paper } from '@mui/material'
-
-
-const colors = generator(6, '02468ACEF')
-const testColors: string[] = []
-
-for (const color of Array.from(colors)) {
-  testColors.push('#' + color)
-}
+import { useEffect, useState } from 'react'
 
 export function ColorsSheet ({slice, setSlice }: {
-    slice: number,
-    setSlice: React.Dispatch<React.SetStateAction<number>>
-  }) {
-  const spliceLimit = 437
-  let i = 0
+  slice: number,
+  setSlice: React.Dispatch<React.SetStateAction<number>>
+}) {
+  const [testColors, setTestColors] = useState([''])
+  const sliceSize = getSliceSize()
 
-  const listColours = testColors.slice(slice * spliceLimit, slice * spliceLimit + spliceLimit).map((color) => {
-      const divStyle = {
-        backgroundColor: color,
-        color: fontColorContrast(color),
-      }
-    return <div key={i++} className='color-block' style={divStyle}>{color}</div>
-  })
+  useEffect(() => {
+    const testColorsResult = getColorsArray()
+    setTestColors(testColorsResult)
+  }, [])
+
+  function listColors () {
+    let i = 0
+    return testColors.slice(slice * sliceSize, slice * sliceSize + sliceSize).map((color) => {
+        const divStyle = {
+          backgroundColor: color,
+          color: fontColorContrast(color),
+        }
+      return <div key={i++} className='color-block' style={divStyle}>{color}</div>
+    })
+  }
 
   return (
     <>
@@ -34,17 +34,31 @@ export function ColorsSheet ({slice, setSlice }: {
         <Slider
           className='slider'
           min={0}
-          max={testColors.length / spliceLimit}
+          max={Math.floor(testColors.length / sliceSize)}
           value={slice}
           setValue={setSlice}
           />
-        <p>Page {Number(slice) + 1} of {(testColors.length / spliceLimit).toFixed(0)} (from {testColors[slice * spliceLimit]} to {testColors[slice * spliceLimit + spliceLimit - 1] || '#FFFFFF'}</p>
+        <p>Page {Number(slice) + 1} of {(testColors.length / sliceSize).toFixed(0)} (from {testColors[slice * sliceSize]} to {testColors[slice * sliceSize + sliceSize - 1] || '#FFF'}</p>
       </div>
-      <Paper className='demo' elevation={3}>
-        <div className='colors-sheet'>
-          {listColours}
-        </div>
+      <Paper className='demo container colors-sheet' elevation={3}>
+        {listColors()}
       </Paper>
     </>
   )
+}
+
+function getColorsArray () {
+  const colors = generator(3, '0123456789ABCDEF')
+  const testColors: string[] = []
+
+  for (const color of Array.from(colors)) {
+    testColors.push('#' + color)
+  }
+
+  return testColors
+}
+
+function getSliceSize () {
+  const spaceLeft = (Number(((window.innerHeight - 48 - 65) / 40).toFixed(0)) - 1)
+  return 16 * spaceLeft
 }
