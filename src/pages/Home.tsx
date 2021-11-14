18a0f74232c07b202aaf4142851e3a55ef3f2929
Eduardo Russo <russoedu@gmail.com>
2021-11-14 06:57:57 +0000

    Fixed links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import { Paper } from "@mui/material";
-import { Code } from "../components/Code";
+import { Paper } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Code } from '../components/Code';
 import './Home.css'
 
 
@@ -9,33 +10,33 @@ export function Home () {
     <Paper className='container' elevation={3}>
       <h1>font-color-contrast</h1>
       <p>
-        <a href="https://www.npmjs.com/package/font-color-contrast">
-          <img src="https://camo.githubusercontent.com/a541dffe52f51f77d240db7203a491f55eaaba2b12cc190456d95696795ee021/68747470733a2f2f696d672e736869656c64732e696f2f6e706d2f762f666f6e742d636f6c6f722d636f6e74726173742e737667" alt="npm" data-canonical-src="https://img.shields.io/npm/v/font-color-contrast.svg"/>
+        <a href='https://www.npmjs.com/package/font-color-contrast' target='_blank' rel='noreferrer'>
+          <img src='https://camo.githubusercontent.com/a541dffe52f51f77d240db7203a491f55eaaba2b12cc190456d95696795ee021/68747470733a2f2f696d672e736869656c64732e696f2f6e706d2f762f666f6e742d636f6c6f722d636f6e74726173742e737667' alt='npm' data-canonical-src='https://img.shields.io/npm/v/font-color-contrast.svg'/>
         </a>
-        <a href="https://github.com/russoedu/font-color-contrast/actions/workflows/main.yml">
-          <img src="https://github.com/russoedu/font-color-contrast/actions/workflows/main.yml/badge.svg" alt="CI Pipeline"/>
+        <a href='https://github.com/russoedu/font-color-contrast/actions/workflows/main.yml' target='_blank' rel='noreferrer'>
+          <img src='https://github.com/russoedu/font-color-contrast/actions/workflows/main.yml/badge.svg' alt='CI Pipeline'/>
         </a>
-        <a href="https://scrutinizer-ci.com/g/russoedu/font-color-contrast/build-status/master">
-          <img src="https://camo.githubusercontent.com/f8a205550f1cdd1c2392196be46394495d5e4966a8c1cf9e15f30bd3057e1227/68747470733a2f2f7363727574696e697a65722d63692e636f6d2f672f727573736f6564752f666f6e742d636f6c6f722d636f6e74726173742f6261646765732f6275696c642e706e673f623d6d6173746572" alt="Build Status" data-canonical-src="https://scrutinizer-ci.com/g/russoedu/font-color-contrast/badges/build.png?b=master"/>
+        <a href='https://scrutinizer-ci.com/g/russoedu/font-color-contrast/build-status/master' target='_blank' rel='noreferrer'>
+          <img src='https://camo.githubusercontent.com/f8a205550f1cdd1c2392196be46394495d5e4966a8c1cf9e15f30bd3057e1227/68747470733a2f2f7363727574696e697a65722d63692e636f6d2f672f727573736f6564752f666f6e742d636f6c6f722d636f6e74726173742f6261646765732f6275696c642e706e673f623d6d6173746572' alt='Build Status' data-canonical-src='https://scrutinizer-ci.com/g/russoedu/font-color-contrast/badges/build.png?b=master'/>
         </a>
-        <a href="https://coveralls.io/github/russoedu/font-color-contrast?branch=ts">
-            <img src="https://camo.githubusercontent.com/e489fcf209af233f6b8e15eca9be16dbf5ebbbcef18ce767d1c10e8076963692/68747470733a2f2f636f766572616c6c732e696f2f7265706f732f6769746875622f727573736f6564752f666f6e742d636f6c6f722d636f6e74726173742f62616467652e7376673f6272616e63683d7473" alt="Coverage Status" data-canonical-src="https://coveralls.io/repos/github/russoedu/font-color-contrast/badge.svg?branch=ts" />
+        <a href='https://coveralls.io/github/russoedu/font-color-contrast?branch=ts' target='_blank' rel='noreferrer'>
+            <img src='https://camo.githubusercontent.com/e489fcf209af233f6b8e15eca9be16dbf5ebbbcef18ce767d1c10e8076963692/68747470733a2f2f636f766572616c6c732e696f2f7265706f732f6769746875622f727573736f6564752f666f6e742d636f6c6f722d636f6e74726173742f62616467652e7376673f6272616e63683d7473' alt='Coverage Status' data-canonical-src='https://coveralls.io/repos/github/russoedu/font-color-contrast/badge.svg?branch=ts' />
         </a>
-        <a href="https://scrutinizer-ci.com/g/russoedu/font-color-contrast/?branch=master">
-          <img src="https://camo.githubusercontent.com/4006117bbba0c578c05fedad4b296974ce1f391fcd45e9e44372255a1118af0c/68747470733a2f2f7363727574696e697a65722d63692e636f6d2f672f727573736f6564752f666f6e742d636f6c6f722d636f6e74726173742f6261646765732f7175616c6974792d73636f72652e706e673f623d6d6173746572" alt="Scrutinizer Code Quality" data-canonical-src="https://scrutinizer-ci.com/g/russoedu/font-color-contrast/badges/quality-score.png?b=master" />
+        <a href='https://scrutinizer-ci.com/g/russoedu/font-color-contrast/?branch=master' target='_blank' rel='noreferrer'>
+          <img src='https://camo.githubusercontent.com/4006117bbba0c578c05fedad4b296974ce1f391fcd45e9e44372255a1118af0c/68747470733a2f2f7363727574696e697a65722d63692e636f6d2f672f727573736f6564752f666f6e742d636f6c6f722d636f6e74726173742f6261646765732f7175616c6974792d73636f72652e706e673f623d6d6173746572' alt='Scrutinizer Code Quality' data-canonical-src='https://scrutinizer-ci.com/g/russoedu/font-color-contrast/badges/quality-score.png?b=master' />
         </a>
-        <a href="https://codeclimate.com/github/russoedu/font-color-contrast">
-          <img src="https://camo.githubusercontent.com/b56e95c32295deb6e822d85e155cd01ce8e9c3e25301cd821d0b48b63dceab39/68747470733a2f2f636f6465636c696d6174652e636f6d2f6769746875622f6477796c2f657374612f6261646765732f6770612e737667" alt="Code Climate" data-canonical-src="https://codeclimate.com/github/dwyl/esta/badges/gpa.svg" />
+        <a href='https://codeclimate.com/github/russoedu/font-color-contrast' target='_blank' rel='noreferrer'>
+          <img src='https://camo.githubusercontent.com/b56e95c32295deb6e822d85e155cd01ce8e9c3e25301cd821d0b48b63dceab39/68747470733a2f2f636f6465636c696d6174652e636f6d2f6769746875622f6477796c2f657374612f6261646765732f6770612e737667' alt='Code Climate' data-canonical-src='https://codeclimate.com/github/dwyl/esta/badges/gpa.svg' />
         </a>
-        <a href="https://snyk.io/test/npm/font-color-contrast">
-          <img src="https://camo.githubusercontent.com/77980cfc13198e616164c295c2ef0ef3fe9d4d4ad0342487847401cd09bfeb02/68747470733a2f2f736e796b2e696f2f746573742f6e706d2f666f6e742d636f6c6f722d636f6e74726173742f62616467652e737667" alt="Known Vulnerabilities" data-canonical-src="https://snyk.io/test/npm/font-color-contrast/badge.svg" />
+        <a href='https://snyk.io/test/npm/font-color-contrast' target='_blank' rel='noreferrer'>
+          <img src='https://camo.githubusercontent.com/77980cfc13198e616164c295c2ef0ef3fe9d4d4ad0342487847401cd09bfeb02/68747470733a2f2f736e796b2e696f2f746573742f6e706d2f666f6e742d636f6c6f722d636f6e74726173742f62616467652e737667' alt='Known Vulnerabilities' data-canonical-src='https://snyk.io/test/npm/font-color-contrast/badge.svg' />
         </a>
       </p>
       <p><strong>font-color-contrast</strong> is a JavaScript module to help you select black or white for a font according to the brightness of the background color to give you the best possible contrast and readability.</p>
       <h2>How does it work</h2>
-      <p><strong>font-color-contrast</strong> uses the algorythm described in the article <a href="https://alienryderflex.com/hsp.html">HSP Color Model — Alternative to HSV (HSB) and HSL</a> where brightness is described as</p>
+      <p><strong>font-color-contrast</strong> uses the algorythm described in the article <a href='https://alienryderflex.com/hsp.html' target='_blank' rel='noreferrer'>HSP Color Model — Alternative to HSV (HSB) and HSL</a> where brightness is described as</p>
       <blockquote>
-        <img alt="brightness = sqrt(0.299 * red^2 + 0.587 * green^2 + 0.114 * blue^2)" src="https://render.githubusercontent.com/render/math?math=brightness=\sqrt{0.299 * red^2 %2B 0.587 * green^2 %2B 0.114 * blue^2}" />
+        <img alt='brightness = sqrt(0.299 * red^2 + 0.587 * green^2 + 0.114 * blue^2)' src='https://render.githubusercontent.com/render/math?math=brightness=\sqrt{0.299 * red^2 %2B 0.587 * green^2 %2B 0.114 * blue^2}' />
       </blockquote>
       <p>Any brightness smaller than 50% means the background is dark.</p>
       <p>Any brightness bigger than 50% means the background is light.</p>
@@ -163,9 +164,9 @@ const fc4 = fontColorContrast(0, 0xcc, 153) // '#000000'
 const fc5 = fontColorContrast(0, 0xcc, 153, 0.7) // '#ffffff'`}
       </Code>
       <h2>Examples</h2>
-      <p>Check the examples on <a href="/sheet">Color sheet demo</a> or on <a href="/picker">Color picker demo</a>.</p>
+      <p>Check the examples on <Link to='/sheet'>Color sheet demo</Link> or on <Link to='/picker'>Color picker demo</Link>.</p>
       <h2>Tests</h2>
-      <p>Tests made using <a href="https://jestjs.io/" rel="nofollow">Jest</a>.</p>
+      <p>Tests made using <a href='https://jestjs.io/' target='_blank' rel='noreferrer'>Jest</a>.</p>
       <h2>Version history</h2>
       <h3>0 -{'>'} 8.1.1</h3>
       <p>JavaScript version, accepting strings for RGB</p>
@@ -173,9 +174,9 @@ const fc5 = fontColorContrast(0, 0xcc, 153, 0.7) // '#ffffff'`}
       <p>TypeScript version.</p>
       <p>Only numbers are now accepted as params when using array or RGB, because it was impossible to know if the string was decimal or hexadecimal. Accepting only numbers we can be sure the correct values are being used to calculate the contrast.</p>
       <h3>9.1.0</h3>
-      <p>Updated algorithm from <a href='https://alienryderflex.com/hsp.html'>https://alienryderflex.com/hsp.html</a> with new thresholds for better contrast.</p>
+      <p>Updated algorithm from <a href='https://alienryderflex.com/hsp.html' target='_blank' rel='noreferrer'>https://alienryderflex.com/hsp.html</a> with new thresholds for better contrast.</p>
       <h3>10.0.0</h3>
-      <p>Included the optional threshold parameter (thanks, <a href='https://github.com/franciscohanna92'>franciscohanna92</a>).</p>
+      <p>Included the optional threshold parameter (thanks, <a href='https://github.com/franciscohanna92' target='_blank' rel='noreferrer'>franciscohanna92</a>).</p>
       <h3>10.0.1</h3>
       <p>Changed target to ES2015</p>
       <h3>10.1.0</h3>
