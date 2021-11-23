ed16cf08aa5ba14bd981b30c579a18ec58be876a
Eduardo Russo <russoedu@gmail.com>
2021-11-23 07:49:12 +0000

    Finished stylying

--- src/components/font.tsx
+++ src/components/font.tsx
@@ -1,24 +1,24 @@
 import { MenuItem } from "@mui/material"
 
 export const fontFamilies = [
-  'Andale Mono',
-  'Arial Black',
-  'Arial',
-  'Comic Sans MS',
-  'Courier',
-  'cursive',
-  'fantasy',
-  'Georgia',
-  'Helvetica',
-  'Impact',
-  'monospace',
-  'Roboto',
-  'sans-serif',
-  'serif',
-  'system-ui',
-  'Times New Roman',
-  'Trebuchet MS',
-  'Verdana',
+  { name: 'Arial', value: 'Arial, Helvetica, sans-serif' },
+  { name: 'Arial Black', value: '"Arial Black", Gadget' },
+  { name: 'Comic Sans', value: '"Comic Sans MS", cursive' },
+  { name: 'Courier New', value: '"Courier New", monospace' },
+  { name: 'Georgia', value: 'Georgia, serif' },
+  { name: 'Impact', value: 'Impact, Charcoal' },
+  { name: 'Lucida Console', value: '"Lucida Console", Monaco' },
+  { name: 'Lucida Sans Unicode', value: '"Lucida Sans Unicode", "Lucida Grande", sans-serif' },
+  { name: 'Palatino Linotype', value: '"Palatino Linotype", "Book Antiqua", Palatino, serif' },
+  { name: 'Tahoma', value: 'Tahoma, Geneva, sans-serif' },
+  { name: 'Times New Roman', value: '"Times New Roman", Times, serif' },
+  { name: 'Trebuchet', value: '"Trebuchet MS", sans-serif' },
+  { name: 'Verdana', value: 'Verdana, Geneva, sans-serif' },
+  { noRead: true, name: 'Symbol', value: 'Symbol' },
+  { noRead: true, name: 'Webdings', value: 'Webdings' },
+  { noRead: true, name: 'Wingdings', value: 'Wingdings' },
+  { name: 'MS Sans Serif', value: '"MS Sans Serif", Geneva, sans-serif' },
+  { name: 'MS Serif', value: '"MS Serif", "New York", serif' },
 ]
 
 export const fontSizes = [
@@ -37,9 +37,24 @@ export const fontSizes = [
 ]
 
 export const fontOptions = fontFamilies.map((font, index) => {
-  return(<MenuItem style={{ fontFamily: font }}key={'font-' + font} value={index}>{font}</MenuItem>)
+  return(
+    <MenuItem
+      style={{ fontFamily: font.noRead ? fontFamilies[0].value : font.value }}
+      key={'font-' + font.name}
+      value={index}
+    >
+      {font.name}
+    </MenuItem>
+  )
 })
 
 export const sizeOptions = fontSizes.map((size, index) => {
-  return(<MenuItem key={'size-' + size} value={index}>{size}</MenuItem>)
+  return(
+    <MenuItem
+      key={'size-' + size}
+      value={index}
+    >
+      {size}
+    </MenuItem>
+  )
 })
